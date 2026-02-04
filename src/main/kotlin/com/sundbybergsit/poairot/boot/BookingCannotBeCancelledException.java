caae18478402f45c152deb2bc07e94a5f029f804
alwa <alix.warnke@gmail.com>
2026-02-04 19:34:46 +0100

    Pruned dead code

--- src/main/kotlin/com/sundbybergsit/poairot/boot/BookingCannotBeCancelledException.java
+++
@@ -1,8 +0,0 @@
-package com.sundbybergsit.poairot.boot;
-
-public class BookingCannotBeCancelledException extends RuntimeException {
-
-    public BookingCannotBeCancelledException(String bookingNumber) {
-        super("Booking " + bookingNumber + " cannot be canceled");
-    }
-}
--- src/main/kotlin/com/sundbybergsit/poairot/boot/BookingNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.sundbybergsit.poairot.boot;
-
-public class BookingNotFoundException extends RuntimeException {
-
-    public BookingNotFoundException(String bookingNumber) {
-        super("Booking " + bookingNumber + " not found");
-    }
-}
